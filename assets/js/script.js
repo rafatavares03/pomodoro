0b3490eb76f507e7cf2ab9119017dd677f1b2ea6
Rafael Tavares <rafatavares03@hotmail.com>
2021-08-29 20:10:35 -0300

    25min and 5min timer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,44 @@ const timerContainer = document.querySelector('#timer');
 const timer = new Date;
 
 function WriteTimer() {
-    
+    const minutes = (timer.getMinutes() < 10) ? '0' + timer.getMinutes() : timer.getMinutes();
+    const seconds = (timer.getSeconds() < 10) ? '0' + timer.getSeconds() : timer.getSeconds();
+    timerContainer.innerHTML = `${minutes}:${seconds}`;
 }
+
+function Pomodoro() {
+    timer.setMinutes(25);
+    timer.setSeconds(0);
+    WriteTimer();
+}
+Pomodoro();
+
+function Break() {
+    timer.setMinutes(5);
+    timer.setSeconds(0);
+    WriteTimer();
+
+}
+
+document.querySelector('#start').addEventListener('click', function Count() {
+    const interval = setInterval(() => {
+        let seconds = timer.getSeconds();
+        seconds--;
+        timer.setSeconds(seconds);
+        WriteTimer();
+        if(timer.getMinutes() === 0 && timer.getSeconds() === 0) {
+            clearInterval(interval);
+            function GoToABreak() {
+                const answer = window.confirm("Do you wan't to do a 5 minutes break?")
+                if (answer === true) {
+                    Break();
+                    Count();
+                } else {
+                    Pomodoro();
+                }
+            }
+            GoToABreak();
+        }
+    }, 1000)
+    
+})
